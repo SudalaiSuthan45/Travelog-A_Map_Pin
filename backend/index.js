7d92947fbf09ff94a4d9092c2fba1f29c625342a
SudalaiSuthan45 <sudalaisuthan3718@gmail.com>
2024-06-08 01:53:11 +0530

    Backend - models and routes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
 
+const userRoute = require ("./routes/users");
+const pinRoute = require ("./routes/pins");
+
 dotenv.config();
+
+app.use(express.json());
 
 mongoose
         .connect(process.env.MONGO_URL)
@@ -15,3 +20,6 @@ mongoose
 app.listen(8800,() => {
     console.log("Backend server is running!")
 })
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
