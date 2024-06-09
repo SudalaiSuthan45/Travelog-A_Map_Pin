0278cdc18c5e0a212834928cefa92a225c6a3968
SudalaiSuthan45 <sudalaisuthan3718@gmail.com>
2024-06-10 02:02:25 +0530

    Frontend - Map,Marker,Popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,68 @@
+import React from 'react';
+import './App.css';
+
+//import './myMapStyles.css';
+import Map, {Marker, Popup} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css'; //Map's css
+
+import RoomIcon from '@mui/icons-material/Room';
+import StarIcon from '@mui/icons-material/Star';
+
 
 
 function App() {
-  return (
-    <div className="App">
-        Hello
+    const [viewport, setViewport] = React.useState({
+      
+      latitude: 48.8584,
+      longitude: 2.2945,
+      zoom: 4
+    });
+  
+    return (
+    
+    <div className='App'>
+
+        <Map
+          {...viewport}
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+          onViewportChange={(nextViewport) => setViewport(nextViewport)}
+          style={{ width: "100vw", height: "100vh" }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        >
+      
+          <Marker latitude={48.8584} longitude={2.2945} offsetLeft={-20} offsetTop={-10} anchor='center'>
+
+            <RoomIcon style={{ fontSize:viewport.zoom * 7, color: "slateblue"}}/>
+
+          </Marker>
+          
+          <Popup longitude={2.2945} latitude={48.8584} anchor="left">
+                <div className="card">
+                  <label>Place</label>
+                    <h4 className="place">Eiffel tower</h4>
+                  <label>Review</label>
+                    <p className="desc"> beatutyyy</p>
+                  <label>Rating</label>
+                  <div className="stars">
+                    <StarIcon className="star"></StarIcon>
+                    <StarIcon className="star"></StarIcon>
+                    <StarIcon className="star"></StarIcon>
+                    <StarIcon className="star"></StarIcon>
+                    <StarIcon className="star"></StarIcon>
+                  </div>
+                  <label>Information</label>
+                    <span className="username">Created by<b>anhs</b></span>
+                    <span className="date">1 hour ago</span>
+                </div>
+          </Popup>
+          
+        </Map>
+
     </div>
+    
   );
 }
+
+
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+// import './index.css'
+// index.js or App.js
+import 'mapbox-gl/dist/mapbox-gl.css';
 import App from './App';
 
 
